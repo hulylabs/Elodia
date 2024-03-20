6188ee5fb303de25c4c1afce7ea9de95ed836fdc
Andrey Platov <andrey@hardcoreeng.com>
2024-03-20 17:52:55 +0100

    checkpoint
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/platform/src/modules/io_ext.ts
+++ packages/platform/src/modules/io_ext.ts
@@ -2,7 +2,7 @@
 // © 2024 Hardcore Engineering, Inc. All Rights Reserved.
 // Licensed under the Eclipse Public License v2.0 (SPDX: EPL-2.0).
 //
-// · platform/io_full.ts
+// · platform/io_ext.ts
 //
 
 class SyncCode<I, O> extends IOBase<I, O> implements SyncIterator<I, O> {
