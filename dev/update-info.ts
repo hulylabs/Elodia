12f9f34dc803db551ebbe923ddd8f0c3d9ad4fae
Andrey Platov <andrey@hardcoreeng.com>
2024-03-21 20:14:31 +0100

    checkpoint
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- dev/update-info.ts
+++ dev/update-info.ts
@@ -1,9 +1,19 @@
 import packageJson from '../package.json'
 import projectInfo from '../project-info.json'
 
-import { Glob, spawnSync } from 'bun'
+const copyrightTs = `
+//
+// © {{year}} {{company}}. All Rights Reserved.
+// Licensed under the {{full_license}} (SPDX: {{license}}).
+//
+// · {{filename}}
+//
 
-const { description, version } = projectInfo['package.json']
+
+`
+
+import { Glob, spawnSync } from 'bun'
+import path from 'node:path'
 
 function constructVersion(): string {
   const date = new Date().toISOString().split('T')[0].replace(/-/g, '')
@@ -16,13 +26,55 @@ function constructVersion(): string {
 }
 
 const newVersion = constructVersion()
+const { description, version } = projectInfo['package.json']
+
+function replacePlaceholders(template: string, placeholderValues: Record<string, string>): string {
+  return template.replace(/{{\s*([\w_]+)\s*}}/g, (match, name) => {
+    // If the placeholder key exists in the provided values, replace it; otherwise, do not replace it.
+    return placeholderValues.hasOwnProperty(name) ? placeholderValues[name] : match
+  })
+}
+
+function removeCommentLines(content: string): string {
+  const lines = content.split('\n')
+
+  let i = 0
+  for (; i < lines.length; i++) {
+    const trimmedLine = lines[i].trim()
+    if (!trimmedLine.startsWith('//') && !trimmedLine.startsWith('/*') && !trimmedLine.startsWith(' *')) {
+      break
+    }
+  }
+  return lines.slice(i).join('\n')
+}
+
+async function processSource(filePath: string): Promise<void> {
+  console.log(`processing source file at '${filePath}'...`)
+
+  const fileContent = await Bun.file(filePath).text()
+  const noCommentContent = removeCommentLines(fileContent)
+
+  const filename = path.basename(filePath)
+  const placeholderValues = {
+    ...projectInfo['package.json'],
+    ...projectInfo.other,
+    version: newVersion,
+    filename,
+  }
+  const newComment = replacePlaceholders(copyrightTs, placeholderValues as any)
+  const newContent = newComment + '\n' + noCommentContent
+
+  await Bun.write(filePath, newContent)
+}
 
 async function processPackage(filePath: string): Promise<void> {
+  const packageDir = path.dirname(filePath)
   try {
     const fileContent = await Bun.file(filePath).text()
     const packageData = JSON.parse(fileContent)
+    const name = packageData.name
 
-    console.log(`processing '${packageData.name}'...`)
+    console.log(`\nprocessing '${name}'...`)
 
     const newPackageData = {
       ...packageData,
@@ -32,8 +84,14 @@ async function processPackage(filePath: string): Promise<void> {
     const updatedPackageJson = JSON.stringify(newPackageData, null, 2)
 
     await Bun.write(filePath, updatedPackageJson)
+
+    console.log(`updating copyrights in source files for '${name}'...`)
+    const glob = new Glob(`**/*.ts`)
+    for await (const file of glob.scan(packageDir)) {
+      await processSource(file)
+    }
   } catch (error) {
-    console.error(`error processing package at ${filePath}`, error)
+    console.error(`error processing package at ${packageDir}`, error)
   }
 }
 
